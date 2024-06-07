66d0260c3c1b3ab6829bfa765ee20fc325bd15ea
Fabio Giovanni Trujillo <f.trujillo2@dxc.com>
2024-06-07 03:40:54 -0600

    Finales de seccion 10, mantenre data

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of, map, delay } from 'rxjs';
+import { catchError, Observable, of, map, tap } from 'rxjs';
 
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
+import { CounterComponent } from '../../../../02-bases/src/app/counter/components/count/counter.component';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
-  constructor(private http: HttpClient ) { }
+  public cacheStore: CacheStore = {
+    byCapital:   { term: '', countries: [] },
+    byCountries: { term: '', countries: [] },
+    byRegion:    { region: '', countries: [] },
+  }
+
+  constructor(private http: HttpClient ) {  }
 
   private getCountriesReques( url: string ): Observable<Country[]> {
     return this.http.get<Country[]>( url )
@@ -30,7 +38,11 @@ export class CountriesService {
 
   searchCapital( term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ term }`;
-    return this.getCountriesReques( url );
+
+    return this.getCountriesReques(url)
+        .pipe(
+          tap( countries => this.cacheStore.byCapital = { term, countries })
+        );
   }
 
   searchCountry( term: string ): Observable<Country[]> {
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime, pipe } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime, pipe } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,9 +7,11 @@ import { Subject, debounceTime, pipe } from 'rxjs';
   styles: [
   ]
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
 
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
+  public initialValue: string = '';
 
   @Input()
   public placeholder: string = '';
@@ -21,13 +23,17 @@ export class SearchBoxComponent implements OnInit {
   public onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSubscription = this.debouncer
     .pipe(
       debounceTime(300)
     )
     .subscribe( value => {
       this.onDebounce.emit( value );
     })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
   }
 
   emitValue( value: string ):void {
