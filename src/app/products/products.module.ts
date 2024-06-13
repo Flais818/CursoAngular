8c380d1342bcd035f5625fb1525c506c93b705f8
Fabio Giovanni Trujillo <f.trujillo2@dxc.com>
2024-06-13 12:47:28 -0600

    Fin de seccion 12: Pipes personalizados

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,20 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { ProductsRoutingModule } from './products-routing.module';
-import { ProductsComponent } from './products.component';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
-import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { OrderComponent } from './pages/order/order.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
-
+import { ProductsComponent } from './products.component';
+import { ProductsRoutingModule } from './products-routing.module';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 @NgModule({
   declarations: [
     ProductsComponent,
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
+    OrderComponent,
+
+    //Pipes
+    ToggleCasePipe,
+    CanFlyPipe,
+    SortByPipe,
   ],
   imports: [
     CommonModule,
